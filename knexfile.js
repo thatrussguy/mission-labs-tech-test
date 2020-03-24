1501533c3a185fb5c7867fc7a16bf3e368c3e619
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2020-03-24 23:55:12 +0000

    switch production database to postgresql

--- knexfile.js
+++ knexfile.js
@@ -13,10 +13,8 @@ module.exports = {
   },
 
   production: {
-    client: "sqlite3",
-    connection: {
-      filename: "./db/prod.sqlite3"
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations"
     },
